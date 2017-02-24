8338aa02d66020e000324614f5b4b9e4fd376b7b
Julio Gajardo <julio.gajardo@mainad.com>
2017-02-24 13:22:07 -0300

    Model Improved

--- model.py
+++ model.py
@@ -5,24 +5,24 @@ import keras.backend as K
 def create_model():
 
     main_input = Input(shape=(6, 7, 1))
-    cnn = Convolution2D(2, 3, 3, border_mode='same')(main_input)
+    cnn = Convolution2D(2, 3, 3, border_mode='same', activation='relu', init='normal')(main_input)
     cnn = MaxPooling2D(pool_size=(1,1), border_mode='same')(cnn)
 
-    cnn = Convolution2D(4, 3, 3, border_mode='same')(cnn)
+    cnn = Convolution2D(4, 3, 3, border_mode='same', activation='relu', init='normal')(cnn)
     cnn = MaxPooling2D(pool_size=(2, 2), border_mode='same')(cnn)
 
-    cnn = Convolution2D(8, 3, 3, border_mode='same')(cnn)
+    cnn = Convolution2D(8, 3, 3, border_mode='same', activation='relu', init='normal')(cnn)
     cnn = MaxPooling2D(pool_size=(1, 1), border_mode='same')(cnn)
 
-    cnn = Convolution2D(16, 3, 3, border_mode='same')(cnn)
+    cnn = Convolution2D(16, 3, 3, border_mode='same', activation='relu', init='normal')(cnn)
     cnn = MaxPooling2D(pool_size=(2, 2), border_mode='same')(cnn)
 
-    cnn = Convolution2D(32, 3, 3, border_mode='same')(cnn)
+    cnn = Convolution2D(32, 3, 3, border_mode='same', activation='relu', init='normal')(cnn)
     cnn = MaxPooling2D(pool_size=(1, 1), border_mode='same')(cnn)
 
     cnn = Flatten()(cnn)
     cnn = Dense(512)(cnn)
-    action = Dense(7, activation='softmax')(cnn)
+    action = Dense(7)(cnn)
     model = Model(input=main_input, output=action)
     model.compile(optimizer='rmsprop', loss='mse')
 
